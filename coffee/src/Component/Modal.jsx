039a30c9be3ae5040cc97d2dfeef98c6dd241486
DINESH S <119110395+Dinesh-122333@users.noreply.github.com>
2025-04-29 15:55:25 +0530

    Update Modal.jsx

--- coffee/src/Component/Modal.jsx
+++ coffee/src/Component/Modal.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Modal = ({ videoUrl, onClose }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-opacity-50 backdrop-blur-md z-50">
-      <div className="relative bg-white p-6 rounded-lg shadow-xl w-1/2 max-w-xl">
+      <div className="relative bg-white p-6 rounded-lg shadow-xl w-[90%] sm:w-3/4 md:w-1/2 lg:w-1/2 max-w-full">
         <button className="absolute top-2 right-2 text-xl bg-amber-200 text-gray-100" onClick={onClose}>
           ✖
         </button>
